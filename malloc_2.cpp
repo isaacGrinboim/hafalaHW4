fe329397f369f2ab619c1ab8534305f12e3152b7
isaacGrinboim <yitzchakg@campus.technion.ac.il>
2023-06-27 14:23:14 +0000

    finished part 2

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <cstring>
 
 int numTags = 0;
 int TagSize = sizeof(MallocMetadata);
@@ -6,7 +7,9 @@ int freeBytes = 0;
 int freeBlocks = 0;
 int allocatedBlocks = 0;
 int allocatedBytes = 0;
+
 Tag* blockListHead = NULL;
+Tag* blockListTail = NULL;
 
 void* realAlloc(size_t size){
     if(size == 0 || size > 1e8){
@@ -27,31 +30,63 @@ void* smalloc(size_t size){
         Tag* currTag = blockListHead;
         while(currTag != NULL){
             if(currTag->size >= size && currTag->is_free){
-                //int realSize = currTag->size;
                 currTag->is_free = false;
-                //currTag->size = size;//
                 freeBlocks--;
-                freeBytes -= (size+TagSize);
-                return currTag + TagSize;
+                freeBytes -= size;
+                return (char*)currTag + TagSize;
             }
             currTag = currTag->next;
         }
-
     void* res = realAlloc(size);
     if(res == NULL){
         return NULL;
     }
-    ((Tag*)res)->size = size;
-    ((Tag*)res)->next = blockListHead;
-    if(blockListHead != nullptr){blockListHead->prev = res;}
-    blockListHead = res;
     ((Tag*)res)->is_free = false;
-
+    ((Tag*)res)->size = size;
+    ((Tag*)res)->next = NULL;
+    ((Tag*)res)->prev = blockListTail;
+    if(blockListHead == NULL){
+        blockListHead = (Tag*)res;
+    } 
+    blockListTail = (Tag*)res;
     numTags++;
     allocatedBlocks++;  
     allocatedBytes += (size+TagSize);
-
-    return res+TagSize;
+    return (char*)res+TagSize;
+}
+void* scalloc(size_t num, size_t size){
+    if(size == 0 || num == 0 || size*num > 1e8){
+        return NULL;
+    }
+    void* res = smalloc(size*num);
+    if(res == NULL){
+        return NULL;
+    }
+    memset(res, 0, size*num);
+    return res;
+}
+void sfree(void* p){
+    if(p != NULL){
+        Tag* tag = (Tag*)((char*)p-TagSize);
+        tag->is_free = true;
+    }
+}
+void* srealloc(void* oldp, size_t size){
+    if(size == 0 || size > 1e8){
+        return NULL;
+    }
+    Tag* oldTag = (Tag*)((char*)oldp - TagSize);
+    if(size <= oldTag->size){
+        return oldp;
+    }
+    void* res = smalloc(size);
+    if(res == NULL){
+        return NULL;
+    }
+    std::memmove(res, oldp, oldTag->size);
+    oldTag->is_free = true;
+    freeBytes += oldTag->size;
+    freeBlocks += 1; 
 }
 
 typedef struct MallocMetadata {
@@ -60,3 +95,23 @@ typedef struct MallocMetadata {
  MallocMetadata* next;
  MallocMetadata* prev;
 }Tag;
+
+
+size_t _num_free_blocks(){
+    return freeBlocks;
+}
+size_t _num_free_bytes(){
+    return freeBytes;
+}
+size_t _num_allocated_blocks(){
+    return allocatedBlocks;
+}
+size_t _num_allocated_bytes(){
+    return allocatedBytes;
+}
+size_t _num_meta_data_bytes(){
+    return TagSize*numTags;
+}
+size_t _size_meta_data(){
+    return TagSize;
+}
