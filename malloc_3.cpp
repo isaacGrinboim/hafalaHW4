2e1c618e626b108aea5d91019e59849ca5c66513
DekelDor94 <102503633+DekelDor94@users.noreply.github.com>
2023-07-09 14:24:05 +0000

    Add files via upload

--- malloc_3.cpp
+++ malloc_3.cpp
@@ -34,11 +34,13 @@ Tag *myBuddy(Tag *tag, size_t size);
 
 Tag *smallestFits(size_t size);
 
-bool checkMyBuddies(Tag *tag, size_t destSize);
+bool checkMyBuddies(Tag *tag, size_t destSize, int *i);
 
 void mmapFree(void *address);
 
 Tag *uniteBuddies(Tag *tag);
+
+Tag *uniteBuddies2(Tag *tag, int *i);
 
 void *mmapAlloc(size_t size);
 
@@ -65,13 +67,13 @@ void *smalloc(size_t size) {
     //(currTag->size + TagSize)/2 - TagSize >= size
     // <=>
     //(currTag->size + TagSize) >= (size + TagSize)*2
-        std::cout<<"smalloc falls" << std::endl;
+
 
     Tag *bestFit = smallestFits(size);
-    std::cout<<"after fall" << std::endl;
+
     if(bestFit == NULL){return NULL;}
 
-    std::cout<<"block size: " << bestFit->size << " | desired size: "<< size << std::endl;
+
     trimmBlock(bestFit, size);
 
     bestFit->is_free = false;
@@ -81,14 +83,14 @@ void *smalloc(size_t size) {
 }
 
 void *scalloc(size_t num, size_t size) {
-    std::cout<<"calloc size: "<< num*size << std::endl;
+
     if (size == 0 || num == 0 || size * num > 1e8) {
         return NULL;
     }
-    std::cout<<"HERE" << std::endl;
+
 
     void *res = smalloc(size * num);
-    std::cout<<"after smalloc" << std::endl;
+
     if (res == NULL) {
         return NULL;
     }
@@ -123,10 +125,12 @@ void *srealloc(void *oldp, size_t size) {
     if (size <= oldTag->size) {
         return oldp;
     }
-    if (checkMyBuddies(oldTag, size)) {
+    int i = 0;
+    if (checkMyBuddies(oldTag, size,  &i)) {
         Tag *oldData = oldTag;
         size_t oldSize = oldTag->size;
-        Tag *startOfBlock = uniteBuddies(oldTag);
+        Tag *startOfBlock = uniteBuddies2(oldTag, &i);
+        //Tag *startOfBlock = uniteBuddies2(oldTag, size);
         std::memmove(startOfBlock, (char *) oldData + TagSize, oldSize);
         return startOfBlock;
     }
@@ -218,7 +222,8 @@ void trimmBlock(Tag *block, size_t size) {
     while (block->size  >= (size+TagSize)*2-TagSize) {
         size_t newSize = ((block->size + TagSize) / 2) - TagSize;// assuming a whole number
 
-        Tag *rightTag = (Tag *) ((char *)block + (newSize + TagSize));
+       Tag *rightTag = (Tag *) ((char *)block + newSize + TagSize);
+       //Tag *rightTag = (Tag *) ((char*)block + (block->size + TagSize)/2);
         if (block == blockListTail) {
             blockListTail = rightTag;
         }
@@ -240,23 +245,21 @@ void trimmBlock(Tag *block, size_t size) {
 
 
 Tag *smallestFits(size_t size) {
-    std::cout<<"smallestFit"<< std::endl;
+
     Tag *current = blockListHead;
     Tag *bestFit = NULL;
 
     while (current != NULL) {
-    std::cout<<"current: "<< current << std::endl;
-    std::cout<<"current->size "<< (Tag*)current->size << std::endl;
-    std::cout<<"size: "<<size << std::endl;
+
         if (current->size >= size && current->is_free) {
             if (bestFit == NULL || (current->size < bestFit->size)) {
                 bestFit = current;
             }
         }
-        std::cout<<"next: "<<current->next << std::endl;
+
         current = current->next;
     }
-    std::cout<<"null? : "<< bestFit << std::endl;
+
     return bestFit;
 }
 
@@ -282,7 +285,7 @@ void *mmapAlloc(size_t size) {
 
 Tag *uniteBuddies(Tag *tag) {
     Tag* current = tag;
-    Tag *buddy = myBuddy(current, current->size);
+    Tag *buddy = myBuddy(current, current->size + TagSize);
     while ( (current->size + TagSize < BLOCKSIZE) && buddy->is_free && buddy->size==current->size) {
         Tag *left = buddy < current ? buddy : current;
         Tag *right = buddy == left ? current : buddy;
@@ -291,7 +294,7 @@ Tag *uniteBuddies(Tag *tag) {
         if (right->next != NULL)
             (right->next)->prev = left;
         current = left;
-        buddy = myBuddy(current, current->size);
+        buddy = myBuddy(current, current->size + TagSize);
 
         numTags--;
         freeBytes += TagSize;
@@ -301,30 +304,82 @@ Tag *uniteBuddies(Tag *tag) {
     }
     return current;
 }
+/*
+Tag *uniteBuddies2(Tag *tag, int &i) {
+    int j = 0;
+    Tag *current = tag;
+    Tag *buddy = myBuddy(current, current->size + TagSize);
+    while ((current->size + TagSize < BLOCKSIZE) && buddy->is_free && buddy->size == current->size && j < i) {
+        Tag *left = buddy < current ? buddy : current;
+        Tag *right = buddy == left ? current : buddy;
+        left->size += TagSize + right->size;
+        left->next = right->next;
+        if (right->next != NULL)
+            (right->next)->prev = left;
+        current = left;
+        buddy = myBuddy(current, current->size + TagSize);
+
+        numTags--;
+        freeBytes += TagSize;
+        freeBlocks--;
+        allocatedBlocks--;
+        allocatedBytes += TagSize;
+        ++j;
+    }
+    return current;
+}*/
+
+Tag *uniteBuddies2(Tag *tag, int* i) {
+    int j = 0;
+    Tag *current = tag;
+    Tag *buddy = myBuddy(current, current->size + TagSize);
+    while ((current->size + TagSize < BLOCKSIZE) && buddy->is_free && buddy->size == current->size && j <= *i) {
+        Tag *left = buddy < current ? buddy : current;
+        Tag *right = buddy == left ? current : buddy;
+        left->size += TagSize + right->size;
+        left->next = right->next;
+        if (right->next != NULL)
+            (right->next)->prev = left;
+        current = left;
+        buddy = myBuddy(current, current->size + TagSize);
+
+        numTags--;
+        freeBytes += TagSize;
+        freeBlocks--;
+        allocatedBlocks--;
+        allocatedBytes += TagSize;
+        ++j;
+    }
+    return current;
+}
+
 
 void mmapFree(void *address) {
     allocatedBlocks--;
     numTags--;
     allocatedBytes -= ((Tag *) address)->size;
 
+
     munmap(address, ((Tag *) address)->size + TagSize);
 }
 
-bool checkMyBuddies(Tag *tag, size_t destSize) {
+bool checkMyBuddies(Tag *tag, size_t destSize, int *i) {
     Tag *curr = tag;
-    Tag *buddy = myBuddy(tag, curr->size);
+    Tag *buddy = myBuddy(tag, curr->size + TagSize);
     size_t size = tag->size;
     while (size < (BLOCKSIZE - TagSize) && buddy->is_free) {
         Tag *left = buddy < tag ? buddy : tag;
         //Tag* right = buddy==left ? tag : buddy ;
         size = (size + buddy->size + TagSize);
-        if (size >= destSize) { return true; }
+        ++(*i);
+        if (size  >= destSize) { return true; }
         curr = left;
-        buddy = myBuddy(curr, size);
+        buddy = myBuddy(curr, size + TagSize);
+
     }
     return false;
 }
 
 Tag *myBuddy(Tag *tag, size_t size) {
-    return (Tag *) ((unsigned long) tag ^ (unsigned long) size);
+    return (Tag *) ((size_t) (tag) ^ size);
 }
